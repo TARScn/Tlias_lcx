bd1f8752f65c017597d9d2d7a5728e0de1910371
TARScn <luocx0301@gmail.com>
2026-05-04 21:16:58 +0800

    update code

--- tlias-web-management/src/main/java/com/lcx/tlias_web_management/controller/DeptController.java
+++ tlias-web-management/src/main/java/com/lcx/tlias_web_management/controller/DeptController.java
@@ -1,10 +1,15 @@
 package com.lcx.tlias_web_management.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
 import com.lcx.tlias_web_management.pojo.Result;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 import java.util.*;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
 import com.lcx.tlias_web_management.pojo.Dept;
 import com.lcx.tlias_web_management.service.DeptService;
 
@@ -14,7 +19,8 @@ public class DeptController {
     @Autowired
     private DeptService deptService;
 
-    @RequestMapping("/depts")    
+    // @RequestMapping(value = "/depts", method = RequestMethod.GET)  
+    @GetMapping("/depts")  
     public Result list() {
         System.out.println("查询全部部门数据");
         List<Dept> deptlist = deptService.findAll();
